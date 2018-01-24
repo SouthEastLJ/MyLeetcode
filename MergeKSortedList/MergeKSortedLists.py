f3ea8125576ffc2bf1d568e301c12356fb75a7c9
LiuJian <sdycliujian@163.com>
2018-01-24 16:39:15 +0800

    Merge K Sorted Lists

--- MergeKSortedList/MergeKSortedLists.py
+++ MergeKSortedList/MergeKSortedLists.py
@@ -7,6 +7,38 @@
 
 
 # Definition for singly-linked list.
+
+#另外一种方案，不用queue，使用
+"""
+def mergeKLists(self, lists):
+        
+        #:type lists: List[ListNode]
+        #:rtype: ListNode
+        
+        
+        pre = cur = ListNode(0)
+           
+        heap = []
+        for i in range(len(lists)):
+            if lists[i]: 
+                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+                
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            cur.next = node[2]  
+            cur = cur.next
+            
+            if cur.next:
+                heapq.heappush(heap, (cur.next.val, idx, cur.next))
+
+        return pre.next
+"""
+
+
+
+
+
 from queue import PriorityQueue
 class ListNode:
 	def __init__(self, x):
